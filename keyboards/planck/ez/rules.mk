392f856953de542995da713f0183950e5679d8c4
Rónán Carrigan <rcarriga@tcd.ie>
2020-04-25 23:02:25 +0100

    Add extra flags to rules https://github.com/qmk/qmk_firmware/issues/8809

--- keyboards/planck/ez/rules.mk
+++ keyboards/planck/ez/rules.mk
@@ -1,6 +1,7 @@
 # MCU name
 MCU = STM32F303
 
+EXTRAFLAGS += --specs=nosys.specs
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
